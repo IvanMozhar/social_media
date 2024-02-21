23d3fa0d0ac3ba5dec7e0facae4c56bce8c59591
Ivan <mozharivan17@gmail.com>
2024-02-21 07:20:22 +0200

    Create views for managing actions with Profile and Post model

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from social_media.models import Profile, Post
+from social_media.permissions import IsOwnerOrReadOnly
+from social_media.serializers import (
+    ProfileSerializer,
+    PostSerializer
+)
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_queryset(self):
+        queryset = Profile.objects.all()
+        username = self.request.query_params.get("username", None)
+        if username is not None:
+            queryset = queryset.filter(username=username)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsOwnerOrReadOnly],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to profile"""
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsOwnerOrReadOnly],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to post"""
+        post = self.get_object()
+        serializer = self.get_serializer(post, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
